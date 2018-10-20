3b24e6f925f59489e38ad5df5d1dffb3703b3fe4
wanxp <977741432@qq.com>
2018-10-20 16:39:25 +0800

    file

--- src/main/java/com/wanxp/App/FileDemo.java
+++ src/main/java/com/wanxp/App/FileDemo.java
@@ -7,8 +7,8 @@ import java.io.*;
  */
 public class FileDemo {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        readFileByLines("D:\\BaiduYunDownload\\20160319\\portal.log.2016031923.txt");
     }
 
     public static void readFileByLines(String fileName) throws IOException {
@@ -17,13 +17,18 @@ public class FileDemo {
         reader = new BufferedReader(new FileReader(file));
         int line = 1;
         String tempString = null;
+        File writename = new File("D:\\BaiduYunDownload\\20160319\\test\\output.txt");
+        writename.createNewFile();
+        BufferedWriter out = new BufferedWriter(new FileWriter(writename));
         while ((tempString = reader.readLine()) != null) {
-            tempString.replaceAll("Ip-address", "")
-                    .replaceAll("pageType","")
-                    .replaceAll("datetime","")
-                    .replaceAll("username","")
-                    .replaceAll("customName","");
-
+            tempString = tempString.replaceAll("Ip-address", " ")
+                    .replaceAll("pageType"," ")
+                    .replaceAll("datetime"," ")
+                    .replaceAll("username"," ")
+                    .replaceAll("customName"," ");
+            out.write(tempString);
         }
+        out.flush();
+        out.close();
     }
 }
