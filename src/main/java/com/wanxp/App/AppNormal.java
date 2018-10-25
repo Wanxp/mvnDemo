55a4a9f7db5d0beb12ec3fe564261032d30196d7
wanxp <977741432@qq.com>
2018-10-25 12:58:20 +0800

    连接池完成部分

--- src/main/java/com/wanxp/App/AppNormal.java
+++ src/main/java/com/wanxp/App/AppNormal.java
@@ -1,16 +1,31 @@
 package com.wanxp.App;
 
+import com.wanxp.App.connecter.PoolManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
 public class AppNormal
 {
-    public static void main( String[] args )
-    {
-        MySQLConnecttor mySQLConnecttor = MySQLConnecttor.getIntstance();
-        mySQLConnecttor.execute("SHOW TABLES");
-        mySQLConnecttor.execute("create table `test` (`id` int(11) default 0, `name` varchar(64) default '');");
-
+    public static void main( String[] args ) throws SQLException {
+        long start = System.currentTimeMillis();
+        PoolManager pool = PoolManager.getInstance();
+        pool.initPool();
+        Connection connection = pool.getConnection();
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE TABLE pv_hour_1 (\n" +
+                "                nas_port_id varchar(125) NOT NULL,\n" +
+                "                ip_address varchar(50),\n" +
+                "                page_type smallint(6) NOT NULL,\n" +
+                "                datetime varchar(30) DEFAULT NULL,\n" +
+                "                username varchar(50) DEFAULT NULL,\n" +
+                "                custom_name varchar(50) DEFAULT NULL\n" +
+                "                PRIMARY KEY (id)\n" +
+                "                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
     }
 }
